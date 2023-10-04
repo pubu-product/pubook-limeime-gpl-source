501d76f55e84584a27e9214bd082e8d4e2785c04
Tony <tony.huang@nuazure.com>
2023-10-04 11:12:36 +0800

    加入 PubookPro 的 Build Type

--- LimeStudio/app/src/main/java/consts/Consts.java
+++ LimeStudio/app/src/main/java/consts/Consts.java
@@ -1,5 +1,7 @@
 package consts;
 
+import net.toload.main.hd.BuildConfig;
+
 public class Consts {
     public static boolean isPubookPro() {
         return BuildConfig.FLAVOR.equals("PubuReaderPro");
